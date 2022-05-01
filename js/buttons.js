eddec765cced3d8d2fa87dcc676ee833f3f818e1
Gareth618 <bloggareth@gmail.com>
2022-05-01 18:45:43 +0300

    improved frontend :medal_sports:

--- js/buttons.js
+++ js/buttons.js
@@ -5,3 +5,16 @@ function onClickBtn(id) {
 function onClick(event) {
   setTimeout(() => event.target.blur(), 900);
 }
+
+function onClickSortBtn(id) {
+  const list = document.getElementById(id).classList;
+  if (list.contains('active')) {
+    list.remove('active');
+  }
+  else {
+    for (const other of ['sort-date', 'sort-likes', 'sort-shares']) {
+      document.getElementById(other).classList.remove('active');
+    }
+    list.add('active');
+  }
+}
