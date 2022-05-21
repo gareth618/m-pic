1faa1878a319f13034d682e89c38741f36a62008
lizzzu <lizudim@yahoo.com>
2022-05-21 15:41:08 +0300

    routing :electric_plug:

---
+++ server.js
@@ -0,0 +1,71 @@
+import { createServer } from 'http';
+import { stat, createReadStream } from 'fs';
+import { readdir } from 'fs/promises';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const resources = [
+  {
+    dir: 'assets/favicons',
+    type: 'image/svg+xml'
+  },
+  {
+    dir: 'assets/photos/avif',
+    type: 'image/avif'
+  },
+  {
+    dir: 'assets/photos/jpg',
+    type: 'image/jpg'
+  },
+  {
+    dir: 'assets/photos/webp',
+    type: 'image/webp'
+  },
+  {
+    dir: 'css',
+    type: 'text/css'
+  },
+  {
+    dir: 'html',
+    type: 'text/html'
+  },
+  {
+    dir: 'js',
+    type: 'application/javascript'
+  }
+];
+
+for (const resource of resources) {
+  const files = await readdir(resource.dir);
+  resource.files = files.map(file => `/${resource.dir}/${file}`);
+}
+
+const server = createServer((req, res) => {
+  console.log(req.url);
+	if (req.url === '/') {
+    render(res, '/html/sign-in.html', 'text/html');
+    return;
+  }
+  for (const resource of resources) {
+    const file = resource.dir === 'html' ? `/html${req.url}.html` : req.url;
+    if (resource.files.includes(file)) {
+      render(res, file, resource.type);
+      return;
+    }
+  }
+  res.statusCode = 404;
+  res.end('Sorry, page not found!');
+});
+
+function render(res, file, type) {
+  stat(`./${file}`, () => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', type);
+    createReadStream(`.${file}`).pipe(res);
+	});
+}
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
