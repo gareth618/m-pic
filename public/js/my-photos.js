3bf1b261327534136e7d0dac2ad58656faffcda8
lizzzu <lizudim@yahoo.com>
2022-06-19 20:20:40 +0300

    css filters :paintbrush:

--- public/js/my-photos.js
+++ public/js/my-photos.js
@@ -48,11 +48,6 @@ window.onload = () => {
     clone.removeAttribute('data-date');
     clone.removeAttribute('data-likes');
     clone.removeAttribute('data-shares');
-    clone.onclick = () => {
-      modal.style.display = 'block';
-      image.src = clone.src;
-      caption.innerHTML = `${clone.alt} ${clone.src} ${clone.title}`;
-    };
     photos.push({
       platform: photo.getAttribute('data-platform'),
       tags: photo.getAttribute('data-tags').split(' '),
@@ -63,6 +58,18 @@ window.onload = () => {
       score: -1,
       id: photos.length
     });
+    const data = photos.slice(-1)[0];
+    clone.onclick = () => {
+      modal.style.display = 'block';
+      image.src = clone.src;
+      caption.innerHTML = `
+        date: ${new Date(data.date).toLocaleString()}<br>
+        platform: ${data.platform}<br>
+        tags: ${data.tags}<br>
+        ${data.likes} likes<br>
+        ${data.shares} shares
+      `;
+    };
   }
   renderPhotos();
   onloadMyPhotos();
@@ -72,3 +79,63 @@ function filterItems(platform) {
   filter = platform;
   sortItems();
 }
+
+const img = document.getElementById('image');
+const resetAll = document.getElementById('resetAll');
+
+const effects = ['brightness', 'contrast', 'blur', 'opacity', 'grayscale', 'saturate', 'sepia', 'hue', 'invert'];
+const effectsInit = {
+  'brightness': 100,
+  'contrast': 100,
+  'saturate': 100,
+  'grayscale': 0,
+  'invert': 0,
+  'hue': 0,
+  'blur': 0,
+  'opacity': 100,
+  'sepia': 0,
+  'dropshadow': 0
+}
+const effectsValues = {
+  'brightness': 100,
+  'contrast': 100,
+  'saturate': 100,
+  'grayscale': 0,
+  'invert': 0,
+  'hue': 0,
+  'blur': 0,
+  'opacity': 100,
+  'sepia': 0,
+  'dropshadow': 0
+};
+
+function updateFilters() {
+  img.style.filter =
+    'brightness(' + effectsValues.brightness + '%) ' +
+    'contrast(' + effectsValues.contrast + '%) ' +
+    'blur(' + effectsValues.blur + 'px) ' +
+    'opacity(' + effectsValues.opacity + '%) ' +
+    'grayscale(' + effectsValues.grayscale + '%) ' +
+    'saturate(' + effectsValues.saturate + '%) ' +
+    'sepia(' + effectsValues.sepia + '%) ' +
+    'hue-rotate(' + effectsValues.hue + 'deg) ' +
+    'invert(' + effectsValues.invert + '%)';
+}
+
+for (const effect of effects) {
+  const slider = document.getElementById(effect);
+  const value = document.getElementById(`${effect}Value`);
+  slider.addEventListener('input', () => {
+    value.innerHTML = slider.value;
+    effectsValues[`${effect}`] = slider.value;
+    updateFilters();
+  });
+}
+
+resetAll.addEventListener('click', () => {
+  for (const effect of effects) {
+    effectsValues[`${effect}`] = effectsInit[`${effect}`];
+    document.getElementById(effect).value = effectsInit[`${effect}`];
+  }
+  updateFilters();
+});
