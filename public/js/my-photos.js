854cdbc1bb1c05aa719f9bfd41b5eb80a73b18b4
lizzzu <lizudim@yahoo.com>
2022-06-19 14:36:38 +0300

    modal images :see_no_evil:

--- public/js/my-photos.js
+++ public/js/my-photos.js
@@ -11,6 +11,11 @@ function renderPhotos() {
 const onloadMyPhotos = window.onload || (() => { });
 window.onload = () => {
   const gallery = document.getElementById('gallery');
+  const modal = document.getElementById('modal');
+  const image = document.getElementById('image');
+  const caption = document.getElementById('caption');
+  const span = document.getElementsByClassName('close')[0];
+  span.onclick = () => modal.style.display = 'none';
   for (const photo of [...gallery.children]) {
     const clone = photo.cloneNode();
     clone.removeAttribute('data-platform');
@@ -18,6 +23,11 @@ window.onload = () => {
     clone.removeAttribute('data-date');
     clone.removeAttribute('data-likes');
     clone.removeAttribute('data-shares');
+    clone.onclick = () => {
+      modal.style.display = 'block';
+      image.src = clone.src;
+      caption.innerHTML = `${clone.alt} ${clone.src} ${clone.title}`;
+    };
     photos.push({
       platform: photo.getAttribute('data-platform'),
       tags: photo.getAttribute('data-tags').split(' '),
